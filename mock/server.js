0629e58a68bfebff693546d0a68a8283adabb38b
Renan Azevedo <renansga2@gmail.com>
2017-01-16 17:55:21 -0200

    Improved json-server to serve static files inside mock/public

--- mock/server.js
+++ mock/server.js
@@ -3,7 +3,9 @@ var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router('./mock/db.json');
 var middlewares = jsonServer.defaults();
+var express = require('express');
 
+server.use('/public', express.static('mock/public'));
 server.use(middlewares);
 server.use(router);
 // In this example, returned resources will be wrapped in a body property
