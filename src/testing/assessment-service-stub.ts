f04c4092fd7497ac0a886e9d017de7f8b6fe1ae0
Renan Azevedo <renansga2@gmail.com>
2017-01-17 16:31:38 -0200

    Fix on AssessmentServiceStub

--- src/testing/assessment-service-stub.ts
+++ src/testing/assessment-service-stub.ts
@@ -26,55 +26,57 @@ export class AssessmentServiceStub {
     let questions = [
       {
         "assessmentId": 1,
-        "id": 1,
-        "text": "If you fly or sit with a great truth, silence absorbs you.",
+        "id": 2,
+        "text": "Wow, courage!Lord, ye cold jack- set sails for adventure! Dozens of anomalies will be lost in plasmas like attitudes in alarms",
+        "media" : [],
         "answers": [
           {
             "id": 1,
-            "text": "Cum lumen assimilant, omnes rationees tractare fortis, neuter urbses."
+            "text": "Simmer mackerel thoroughly, then mix with milk and serve carefully in wok."
           },
           {
             "id": 2,
-            "text": "Place the margerine in a fine-mesh strainer, and flavor fairly with divided BBQ sauce."
+            "text": "Peace at the homeworld was the alarm of starlight travel, arrested to an extraterrestrial hur'q."
           },
           {
             "id": 3,
-            "text": "Oh, never lead a shark."
+            "text": "Where is the golden gull."
           },
           {
             "id": 4,
-            "text": "The collision course is a distant space suit."
+            "text": "Cum adelphis tolerare, omnes resistentiaes manifestum nobilis, domesticus decores."
           },
           {
             "id": 5,
-            "text": "Talis, nobilis adiurators acceleratrix experientia de flavum, festus lapsus."
+            "text": "Dead, colorful ships patiently examine a reliable, vital space suit."
           }
         ]
       },
       {
         "assessmentId": 1,
-        "id": 2,
-        "text": "Wow, courage!Lord, ye cold jack- set sails for adventure! Dozens of anomalies will be lost in plasmas like attitudes in alarms",
+        "id": 3,
+        "text": "Consectetur adipisicing elit. Ab autem ducimus ea fuga nesciunt nulla sed voluptatibus?",
+        "media": [],
         "answers": [
           {
             "id": 1,
-            "text": "Cum lumen assimilant, omnes rationees tractare fortis, neuter urbses."
+            "text": "Per guest prepare one container of ricotta with chopped celery for dessert."
           },
           {
             "id": 2,
-            "text": "Place the margerine in a fine-mesh strainer, and flavor fairly with divided BBQ sauce."
+            "text": "Leek mousse has to have a shredded, sour okra component."
           },
           {
             "id": 3,
-            "text": "Oh, never lead a shark."
+            "text": "Moons yell with coordinates at the senior radiation dome!"
           },
           {
             "id": 4,
-            "text": "The collision course is a distant space suit."
+            "text": "If you disappear or occur with a superior dogma, chaos absorbs you."
           },
           {
             "id": 5,
-            "text": "Talis, nobilis adiurators acceleratrix experientia de flavum, festus lapsus."
+            "text": "Pol, varius triticum!"
           }
         ]
       }
