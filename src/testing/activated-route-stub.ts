f7060869a9214fa3f161ab9ebe3ed3d5d3c9cea7
Stanley Yoshinori Takamatsu <stanleytakamatsu@gmail.com>
2017-03-11 20:51:09 -0300

    Fix the wrong Injectable decorator

--- src/testing/activated-route-stub.ts
+++ src/testing/activated-route-stub.ts
@@ -1,7 +1,5 @@
-import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-@Injectable()
 export class ActivatedRouteStub {
 
   // ActivatedRoute.params is Observable
