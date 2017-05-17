23f36d7b6ed2b8bfe32ae2f962f2fee2ec46f809
Renan Azevedo <renansga2@gmail.com>
2017-05-17 14:44:24 -0300

    Add support for html attr on item options

--- src/app/applyment/question-page/answer.component.ts
+++ src/app/applyment/question-page/answer.component.ts
@@ -1,20 +1,26 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
 import { Option } from '../../shared/model/option';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'qp-answer',
   templateUrl: './answer.component.html',
-  styleUrls: ['./answer.component.sass']
+  styleUrls: ['./answer.component.sass'],
 })
-export class AnswerComponent {
+export class AnswerComponent implements OnInit {
   @Input() option: Option = { id: 0, text: '' };
   @Input() checked = false;
   @Output() onClicked = new EventEmitter<number>();
+  optionText: SafeHtml = null;
 
-  constructor () {
+  constructor(private _sanitizer: DomSanitizer) {
   }
 
-  onClick () {
+  ngOnInit(): void {
+    this.optionText = this._sanitizer.bypassSecurityTrustHtml(this.option.text);
+  }
+
+  onClick() {
     this.checked = true;
     this.onClicked.emit(this.option.id);
   }
