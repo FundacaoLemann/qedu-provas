ca1bab37a8981550fb2abf41ef413f89246e9e57
Renan Azevedo <renansga2@gmail.com>
2018-03-16 15:59:13 -0300

    Add tests

--- src/app/applyment/question-page/answer-manager.service.spec.ts
+++ src/app/applyment/question-page/answer-manager.service.spec.ts
@@ -25,20 +25,21 @@ describe('AnswerManagerService', () => {
   );
 
   it(
-    "item's visualizedTime should be incremented by 1",
-    async(
+    `item's visualizedTime should be incremented by 1`,
+    fakeAsync(
       inject([AnswerManagerService], (service: AnswerManagerService) => {
         const answer = new Answer({});
         service.register(answer).subscribe((updatedAnswer: Answer) => {
           expect(updatedAnswer.visualizedTimes).toEqual(1);
         });
+        discardPeriodicTasks();
       }),
     ),
   );
 
   it(
     'updates optionId and emit',
-    async(
+    fakeAsync(
       inject([AnswerManagerService], (service: AnswerManagerService) => {
         const answer = new Answer();
         const expectedAnswers = [
@@ -52,6 +53,8 @@ describe('AnswerManagerService', () => {
 
         service.setOption(2);
         expect(answers).toEqual(expectedAnswers);
+
+        discardPeriodicTasks();
       }),
     ),
   );
@@ -65,13 +68,64 @@ describe('AnswerManagerService', () => {
 
         service
           .register(answer)
-          .subscribe(emittedAnswer => lastEmittedAnswer = emittedAnswer);
+          .subscribe(emittedAnswer => (lastEmittedAnswer = emittedAnswer));
 
         tick(3000);
-        expect(lastEmittedAnswer).toEqual(new Answer({
-          visualizedTimes: 1,
-          spentTimeInSeconds: 3,
-        }));
+        expect(lastEmittedAnswer).toEqual(
+          new Answer({
+            visualizedTimes: 1,
+            spentTimeInSeconds: 3,
+          }),
+        );
+        discardPeriodicTasks();
+      }),
+    ),
+  );
+
+  it(
+    'switch between answers',
+    fakeAsync(
+      inject([AnswerManagerService], (service: AnswerManagerService) => {
+        let answer1 = new Answer({ itemId: '1' });
+        let answer2 = new Answer({ itemId: '2' });
+
+        // First tracking
+        service
+          .register(answer1)
+          .subscribe(updateAnswer => (answer1 = updateAnswer));
+        tick(1000);
+        service.setOption(2);
+        tick(7000);
+
+        // Change to track answer 2
+        service
+          .register(answer2)
+          .subscribe(updatedAnswer => (answer2 = updatedAnswer));
+        tick(10000);
+
+        // Back to answer1
+        service
+          .register(answer1)
+          .subscribe(updatedAnswer => answer1 = updatedAnswer);
+        tick(3000);
+        service.setOption(4);
+
+        expect(answer1).toEqual(
+          new Answer({
+            itemId: '1',
+            optionId: 4,
+            visualizedTimes: 2,
+            spentTimeInSeconds: 11,
+          }),
+        );
+        expect(answer2).toEqual(
+          new Answer({
+            itemId: '2',
+            optionId: 0,
+            visualizedTimes: 1,
+            spentTimeInSeconds: 10,
+          }),
+        );
         discardPeriodicTasks();
       }),
     ),
