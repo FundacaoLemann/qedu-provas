7affe760d677344be49d8e3bbead810d7e76aa86
Renan Azevedo <renansga2@gmail.com>
2018-05-10 20:34:19 -0300

    Create lite version of app

---
+++ src/app-lite/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qp-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
