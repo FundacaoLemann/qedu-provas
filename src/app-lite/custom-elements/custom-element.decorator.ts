3bffb5fd2d7eb6d1623e8443f2ecb6cb14d9366e
Renan Azevedo <renansga2@gmail.com>
2018-05-17 14:23:30 -0300

    Improve custom elements

--- src/app-lite/custom-elements/custom-element.decorator.ts
+++ src/app-lite/custom-elements/custom-element.decorator.ts
@@ -1,10 +1,10 @@
-// import 'reflect-metadata';
+import { getAnnotations } from './get-annotations.function';
+
 export function CustomElement(customTagName: string): ClassDecorator {
-  return function (constructor: any) {
-    const annotations = constructor.__annotations__.slice();
-    annotations[0].customTagName = customTagName;
+  return function(constructor: any) {
+    const annotations = getAnnotations(constructor);
+    annotations.customTagName = customTagName;
 
     Object.defineProperty(constructor, '__annotations__', annotations);
   };
 }
-
