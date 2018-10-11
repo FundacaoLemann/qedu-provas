8766c5cd8ea4130e87514f3fe5ebd7f12aba8165
Renan Azevedo <renansga2@gmail.com>
2018-10-11 13:36:35 -0300

    Add PanelPageComponent

--- src/app/shared/components/panel-page/panel-page.component.ts
+++ src/app/shared/components/panel-page/panel-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, AfterViewInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'qp-panel-page',
   templateUrl: './panel-page.component.html',
-  styleUrls: ['./panel-page.component.sass']
+  styleUrls: ['./panel-page.component.sass'],
 })
-export class PanelPageComponent implements OnInit {
+export class PanelPageComponent implements OnInit, AfterViewInit {
+  @ViewChild('footerContainer') private footer: ElementRef<HTMLElement>;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    // console.log(this.footer);
+  }
+
+  footerStyle(): string {
+    if (!this.footer) {
+      return 'none';
+    }
+
+    return this.footer.nativeElement.innerHTML.trim() ? 'block' : 'none';
   }
 
 }
