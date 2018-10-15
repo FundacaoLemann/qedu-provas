aedda167864b7d4829f8bf722a747414c100e57b
Renan Azevedo <renansga2@gmail.com>
2018-10-15 12:42:16 -0300

    Display error messages

--- src/app/core/services/matrix/matrix.service.ts
+++ src/app/core/services/matrix/matrix.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { map } from 'rxjs/operators';
 
 
@@ -32,6 +33,7 @@ export class MatrixService extends RequestService {
       .get<GetMatrixResponseDTO>(requestUrl)
       .pipe(
         map(this.formatGetMatrixResponse.bind(this)),
+        catchError(this.handleError)
       );
   }
 
