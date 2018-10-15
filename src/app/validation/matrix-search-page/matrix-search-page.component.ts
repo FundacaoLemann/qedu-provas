683c3c2bfb030c1af09d76cde1a7da96cab648a1
Renan Azevedo <renansga2@gmail.com>
2018-10-15 12:42:16 -0300

    Display error messages

--- src/app/validation/matrix-search-page/matrix-search-page.component.ts
+++ src/app/validation/matrix-search-page/matrix-search-page.component.ts
@@ -1,22 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+
 import { MatrixService } from '../../core/services/matrix/matrix.service';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'qp-matrix-search-page',
   templateUrl: './matrix-search-page.component.html',
-  styleUrls: ['./matrix-search-page.component.sass']
+  styleUrls: ['./matrix-search-page.component.sass'],
 })
 export class MatrixSearchPageComponent implements OnInit {
   public matrixId = '';
+  public error = '';
+  public submitting = false;
 
-  constructor(private matrixService: MatrixService) { }
+  constructor(private matrixService: MatrixService) {
+  }
 
   ngOnInit() {
   }
 
-  handleSubmit(event: Event) {
+  handleSubmit() {
+    if (!this.matrixId) {
+      return;
+    }
+
+    this.setSubmitting(true);
     this.matrixService
       .getMatrix({ id: this.matrixId })
-      .subscribe(data => console.log(data));
+      .pipe(finalize(this.finalizeRequest))
+      .subscribe(
+        () => {},
+        this.setError,
+      );
+  }
+
+  setError = (error: Error) => {
+    this.error = error.message;
+  }
+
+  setSubmitting = (state: boolean) => {
+    this.submitting = state;
+  }
+
+  finalizeRequest = () => {
+    this.setSubmitting(false);
   }
 }
