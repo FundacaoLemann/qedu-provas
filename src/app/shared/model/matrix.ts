82fe9b202e94cd4313b995803dd7980f507c56cd
Renan Azevedo <renansga2@gmail.com>
2018-10-18 19:08:42 -0300

    Add item visualization

--- src/app/shared/model/matrix.ts
+++ src/app/shared/model/matrix.ts
@@ -7,4 +7,20 @@ export class Matrix {
   grade: number;
   subjects: string[];
   items: Item[];
+
+  constructor({
+    id = '',
+    title = '',
+    numberOfItems = 0,
+    grade = 0,
+    subjects = [],
+    items = [],
+  } = {}) {
+    this.id = id;
+    this.title = title;
+    this.numberOfItems = numberOfItems;
+    this.grade = grade;
+    this.subjects = subjects;
+    this.items = items;
+  }
 }
--- src/app/validation/pages/item-page/item-page.component.ts
+++ src/app/validation/pages/item-page/item-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 import { Item } from '../../../shared/model/item';
+import { Matrix } from '../../../shared/model/matrix';
 import { ValidationStateService } from '../../services/validation-state.service';
 
 @Component({
@@ -12,28 +12,46 @@ import { ValidationStateService } from '../../services/validation-state.service'
 })
 export class ItemPageComponent implements OnInit {
   title = '';
-  questionIndex = 0;
-  question = new Item();
+  matrix = new Matrix();
+  currentItem = new Item();
+  currentItemIndex = 0;
   answer = 0;
+  itemsLength = 1;
 
-  constructor(private stateService: ValidationStateService,
-              private route: ActivatedRoute) {
+  constructor(
+    private stateService: ValidationStateService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
+
+  async ngOnInit() {
+    if (!this.stateIsValid()) {
+      return this.redirectOnEmptyState();
+    }
+
+    const { matrix } = this.stateService.state;
+    this.matrix = matrix;
+    this.title = matrix.title;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.currentItemIndex = +params.get('itemIndex');
+      this.currentItem = matrix.items[this.currentItemIndex - 1];
+      this.itemsLength = matrix.items.length;
+    });
   }
 
-  ngOnInit() {
-    const { state } = this.stateService;
+  stateIsValid = (): boolean => {
+    return !!this.stateService.state.matrix;
+  }
 
-    this.title = state.matrix.title;
-    this.route.paramMap.subscribe(
-      (params: ParamMap) => {
-        this.questionIndex = +params.get('itemIndex') - 1;
-        this.question = state.matrix.items[this.questionIndex];
-      },
-    );
+  redirectOnEmptyState = () => {
+    return this.router.navigateByUrl('/validacao');
+  }
+
+  handleNextQuestion = () => {
+    this.router.navigate(['/validacao', this.matrix.id, 'item', (this.currentItemIndex + 1)]);
   }
 
   handleOptionClick = () => {
     console.log('clicked');
   }
-
 }
