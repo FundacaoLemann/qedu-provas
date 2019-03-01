5314fb64724884fab160630529cb34ea0e27f0e3
CJ Patoilo <cjpatoilo@gmail.com>
2019-03-01 10:43:20 -0300

    Move logic to service

--- src/app/applyment/pages/troubleshoot-page/troubleshoot-page.component.spec.ts
+++ src/app/applyment/pages/troubleshoot-page/troubleshoot-page.component.spec.ts
@@ -1,7 +1,13 @@
+import { By } from '@angular/platform-browser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppModule } from '../../../app.module';
 import { TroubleshootPageComponent } from './troubleshoot-page.component';
+
+const browserMock = {
+  name: 'Chrome',
+  version: 73
+};
 
 describe('TroubleshootPageComponent', () => {
   let component: TroubleshootPageComponent;
@@ -15,15 +21,27 @@ describe('TroubleshootPageComponent', () => {
         ],
       })
       .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(TroubleshootPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
-  it('should create', () => {
+  it('should create', async(() => {
     expect(component).toBeTruthy();
-  });
+  }));
+
+  it('should display the browser name and version', async(() => {
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const text = `Chrome 72 (navegador suportado)`;
+    const title = fixture
+      .debugElement
+      .query(By.css('.troubleshoot-title'))
+      .nativeElement
+      .innerText;
+
+    expect(title).toEqual(text);
+  }));
 });
--- src/app/applyment/pages/troubleshoot-page/troubleshoot-page.component.ts
+++ src/app/applyment/pages/troubleshoot-page/troubleshoot-page.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from '@angular/core';
+import {TroubleshootService, BrowserInterface} from './troubleshoot-page.service';
 
 @Component({
   selector: 'qp-troubleshoot-page',
   templateUrl: './troubleshoot-page.component.html',
   styleUrls: ['./troubleshoot-page.component.sass'],
+  providers: [TroubleshootService],
 })
 export class TroubleshootPageComponent implements OnInit {
   isBrowserSupported = true;
@@ -11,69 +13,17 @@ export class TroubleshootPageComponent implements OnInit {
     name: 'Buscando informações sobre seu navegador',
     version: 0,
   };
-  recommendedBrowsers: BrowserInterface[] = [
-    {name: 'Chrome', version: 67},
-    {name: 'Firefox', version: 60},
-  ];
+  recommendedBrowsers: BrowserInterface[];
 
-  constructor() {
+  constructor(
+    private troubleshootService: TroubleshootService,
+  ) {
   }
 
   ngOnInit() {
-    const browserNameAndVersion = this.getBrowserNameAndVersion(window.navigator.userAgent);
-    this.currentBrowser = this.splitNameAndVersion(browserNameAndVersion);
-    this.isBrowserSupported = this.browserHasSupport(this.currentBrowser);
+    const browserNameAndVersion = this.troubleshootService.getBrowserNameAndVersion(window.navigator.userAgent);
+    this.currentBrowser = this.troubleshootService.splitNameAndVersion(browserNameAndVersion);
+    this.isBrowserSupported = this.troubleshootService.browserHasSupport(this.currentBrowser);
+    this.recommendedBrowsers = this.troubleshootService.recommendedBrowsers;
   }
-
-  browserHasSupport(currentBrowser: BrowserInterface): boolean {
-    const result = this.recommendedBrowsers
-      .filter(({name}) => name === currentBrowser.name)
-      .filter(({version}) => version > currentBrowser.version);
-
-    return result.length
-      ? true
-      : false;
-  }
-
-  splitNameAndVersion(browser: string): BrowserInterface {
-    const name: string = browser.split(' ')[0];
-    const version: string = browser.split(' ')[1];
-
-    return ({
-      name,
-      version: parseInt(version, 0),
-    });
-  }
-
-  getBrowserNameAndVersion(userAgent): string {
-    let userAgentMatch = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    let temp;
-
-    if (/trident/i.test(userAgentMatch[1])) {
-      temp = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
-      return 'IE ' + (temp[1] || '');
-    }
-
-    if (userAgentMatch[1] === 'Chrome') {
-      temp = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
-      if (temp !== null) {
-        return temp.slice(1).join(' ').replace('OPR', 'Opera');
-      }
-    }
-
-    userAgentMatch = userAgentMatch[2]
-      ? [userAgentMatch[1], userAgentMatch[2]]
-      : [navigator.appName, navigator.appVersion, '-?'];
-
-    if ((temp = userAgent.match(/version\/(\d+)/i)) !== null) {
-      userAgentMatch.splice(1, 1, temp[1]);
-    }
-
-    return userAgentMatch.join(' ');
-  }
-}
-
-interface BrowserInterface {
-  name: string;
-  version: number;
 }
